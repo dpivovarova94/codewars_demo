d4b8bfe82f53b3436b2aeed83c2a235b59b8db0d
Darima Pivovarova <dpivovarova@gmail.com>
2023-07-10 15:59:17 +0200

    descending order

---
+++ 7kyu/descending-order.rb
@@ -0,0 +1,22 @@
+# DESCRIPTION:
+# Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
+# Essentially, rearrange the digits to create the highest possible number.
+
+# Examples:
+# Input: 42145 Output: 54421
+
+# Input: 145263 Output: 654321
+
+# Input: 123456789 Output: 987654321
+def descending_order(n)
+  #...
+  n.digits.join.to_i
+end
+
+def descending_order(n)
+  n.to_s.chars.map { |digit| digit.to_i }.sort.reverse.join.to_i
+end
+
+def descending_order(n)
+  n.to_s.chars.sort.reverse.join.to_i
+end
